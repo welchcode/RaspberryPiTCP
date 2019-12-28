a6dc1c34bf0ddd60d194d48f5dd7f2a83822e0b6
James Welch <welch.james@ic.edu>
2019-12-28 14:44:30 -0600

    TCP Client that allows for an arg message to be passed in. Simply change the 'server_hostname' var to the IP of your raspberry PI on your network

--- TCPClient.py
+++ TCPClient.py
@@ -1,10 +1,17 @@
 import socket
+import sys
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-message = "I am the client"
+if(len(sys.argv) == 2):
+    message = str(sys.argv[1])
+else:
+    message = 'Arg length of ' + str(len(sys.argv)) + ' is not correct, arg length must be exactly 2 ---> the first arg is the script, and the second is the message'
 
-client.connect(('localhost', 7500))
-client.sendto(message.encode(), ('localhost', 7500))
+#raspberryPi's IP on the network
+server_hostname = '192.168.1.12'
+
+client.connect((server_hostname, 7500))
+client.sendto(message.encode(), (server_hostname, 7500))
 
 client.close()
